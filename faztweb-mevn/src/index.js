8b86f2ad94a20cc32d60ae4a38adeca38bc59477
Carlos Soto <odoo.soporte@gmail.com>
2020-08-14 19:19:29 -0400

    Instalacion morgan js

--- faztweb-mevn/src/index.js
+++ faztweb-mevn/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 // Settings
 app.set('port', process.env.PORT || 3000);
 
 // Middlewares
+app.use(morgan('dev'));
 
 // Routes
 
